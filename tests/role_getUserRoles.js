7a709e5b86445d35a03aac0a89c9e87c07e5e1f1
brainshower <matt@mattgenovese.net>
2014-10-05 19:05:24 +0000

    Role updates finished.

---
+++ tests/role_getUserRoles.js
@@ -0,0 +1,25 @@
+var roles = require("../users/role");
+var logger = require("../global/logger");
+var dbopen = require("../global/dbopen");
+var globals = require("../global/globals");
+
+//var dbname = globals.dbname;
+var dbname = "test";
+
+
+logger.log.info("Testing getUserRoles...");
+dbopen.openDB(dbname).then(
+
+    function (dbObj) {
+        roles.getUserRoles({username:"mgenovese"}).then(
+            function (success) {
+                logger.log.info("Success, bitches!", success);
+            },
+            function (fail) {
+                logger.log.warn("Failed, bitches!", fail);
+            }
+        );
+
+  }
+
+);
